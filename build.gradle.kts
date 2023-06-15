7b737f855a0577a14970cbc333662301fa937cfa
aesperer <piyrw9754@gmail.com>
2023-06-15 11:10:34 +0900

    add :: redis dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
 
